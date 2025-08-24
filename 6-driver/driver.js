34e6b3d8fe84d1d68b7510d650ddd7c068bc78c8
Полина Ахметзянова <lina@MacBook-Air-Polina.local>
2025-08-24 17:50:39 +0300

    Changed

--- 6-driver/driver.js
+++ 6-driver/driver.js
@@ -2,4 +2,4 @@ let age = Number(prompt('Введи свой возраст: '));
 const isDrunk = true;
 const hasLicence = true;
 
-console.log(`Может ли вести машину? ${(hasLicence && age >= 18 && !isDrunk) ? 'Может' : 'Не может'}`);
+console.log(`Может ли вести машину? ${(hasLicence && age >= 18 && !isDrunk) ? 'может' : 'не может'}`);
